456fcc1407d714d4f259354bb4275ce390f1a35d
nico <brun.nicolas21@gmail.com>
2017-12-12 20:04:15 +0100

    rewritting

--- cbor.go
+++ cbor.go
@@ -1,30 +1,48 @@
 package cbor
 
 import (
-	"errors"
+	"bytes"
+	"encoding/binary"
 	"fmt"
+	"io"
+	"math"
+	"net/url"
 	"reflect"
+	"regexp"
+	"time"
+	"unicode/utf8"
 )
 
-//Major types
 const (
 	Uint byte = iota << 5
 	Int
 	Bin
 	String
-	Slice
+	Array
 	Map
 	Tag
 	Other
 )
 
-//Optional Tagging items
 const (
-	IsoTime  = 0x00
-	UnixTime = 0x01
-	Item     = 0x18
-	URI      = 0x20
-	Regex    = 0x23
+	False byte = iota + 20
+	True
+	Nil
+	Undefined
+)
+
+const (
+	Float16 byte = iota + 25
+	Float32
+	Float64
+)
+
+const (
+	IsoTime  byte = 0x00
+	UnixTime byte = 0x01
+	Item     byte = 0x18
+	URI      byte = 0x20
+	Regex    byte = 0x23
 )
 
 const (
@@ -34,45 +52,189 @@ const (
 	Len8
 )
 
-const (
-	False = 20 + iota
-	True
-	Nil
-	Undefined
-)
-
-const (
-	Float16 = 25 + iota
-	Float32
-	Float64
-)
-
-const (
-	Indef = 0x1F
-	Stop  = 0xFF
-)
-
-const mask = 0x1F
-
-type InvalidTagErr byte
-
-func (i InvalidTagErr) Error() string {
-	return fmt.Sprintf("invalid tag found %#02x", byte(i))
+func Marshal(v interface{}) ([]byte, error) {
+	b := new(bytes.Buffer)
+	if err := marshal(b, reflect.ValueOf(v)); err != nil {
+		return nil, err
+	}
+	return b.Bytes(), nil
 }
 
-type UnassignedTagErr byte
-
-func (u UnassignedTagErr) Error() string {
-	return fmt.Sprintf("unassigned tag %#02x", byte(u))
+func Unmarshal(bs []byte, v interface{}) error {
+	return unmarshal(bytes.NewReader(bs), reflect.ValueOf(v).Elem())
 }
 
-type UnsupportedTypeErr reflect.Kind
-
-func (u UnsupportedTypeErr) Error() string {
-	return fmt.Sprintf("cbor: %s not supported", reflect.Kind(u))
+func unmarshal(b *bytes.Reader, v reflect.Value) error {
+	return nil
 }
 
-var (
-	ErrTooManyValues = errors.New("cbor: too many values")
-	ErrTooFewValues  = errors.New("cbor: too few values")
-)
+func marshal(b *bytes.Buffer, v reflect.Value) error {
+	switch v.Kind() {
+	default:
+		return fmt.Errorf("cbor: %s unsupported data type", v.Kind())
+	case reflect.Invalid:
+		binary.Write(b, binary.BigEndian, Other|Undefined)
+	case reflect.Ptr:
+		if v.IsNil() {
+			binary.Write(b, binary.BigEndian, Other|Nil)
+		} else {
+			return marshal(b, v.Elem())
+		}
+	case reflect.Interface:
+		return marshal(b, reflect.ValueOf(v.Interface()))
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		return encodeNumber(b, Uint, v.Uint())
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		if i := v.Int(); i >= 0 {
+			return encodeNumber(b, Uint, uint64(i))
+		} else {
+			return encodeNumber(b, Int, uint64(-i-1))
+		}
+	case reflect.Float32:
+		v := math.Float32bits(float32(v.Float()))
+		binary.Write(b, binary.BigEndian, Other|Float32)
+		binary.Write(b, binary.BigEndian, v)
+	case reflect.Float64:
+		v := math.Float64bits(v.Float())
+		binary.Write(b, binary.BigEndian, Other|Float64)
+		binary.Write(b, binary.BigEndian, v)
+	case reflect.Bool:
+		i := byte(Other | False)
+		if v.Bool() {
+			i = byte(Other | True)
+		}
+		binary.Write(b, binary.BigEndian, i)
+	case reflect.String:
+		s, t := v.String(), String
+		if !utf8.ValidString(s) {
+			t = Bin
+		}
+		if err := encodeString(b, t, s); err != nil {
+			return err
+		}
+	case reflect.Slice, reflect.Array:
+		z := v.Len()
+		if err := encodeLength(b, Array, uint64(z)); err != nil {
+			return err
+		}
+		for i := 0; i < z; i++ {
+			if err := marshal(b, v.Index(i)); err != nil {
+				return err
+			}
+		}
+	case reflect.Map:
+		z := v.Len()
+		if err := encodeLength(b, Map, uint64(z)); err != nil {
+			return err
+		}
+		for i, vs := 0, v.MapKeys(); i < z; i++ {
+			if err := marshal(b, vs[i]); err != nil {
+				return err
+			}
+			if err := marshal(b, v.MapIndex(vs[i])); err != nil {
+				return err
+			}
+		}
+	case reflect.Struct:
+		if ok, err := encodeItem(b, v.Interface()); ok || err != nil {
+			return err
+		}
+		z, t := v.NumField(), v.Type()
+		if err := encodeLength(b, Map, uint64(z)); err != nil {
+			return err
+		}
+		for i := 0; i < z; i++ {
+			f := t.Field(i)
+			if len(f.PkgPath) > 0 {
+				continue
+			}
+			n := f.Name
+			if t := f.Tag.Get("cbor"); len(t) > 0 {
+				n = t
+			}
+			if err := encodeString(b, String, n); err != nil {
+				return err
+			}
+			if err := marshal(b, v.Field(i)); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func encodeItem(w io.Writer, v interface{}) (bool, error) {
+	var s string
+	switch v := v.(type) {
+	default:
+		return false, nil
+	case time.Time:
+		binary.Write(w, binary.BigEndian, Tag|IsoTime)
+		s = v.UTC().Format(time.RFC3339)
+	case url.URL:
+		binary.Write(w, binary.BigEndian, Tag|Item)
+		binary.Write(w, binary.BigEndian, URI)
+		s = v.String()
+	case regexp.Regexp:
+		binary.Write(w, binary.BigEndian, Tag|Item)
+		binary.Write(w, binary.BigEndian, Regex)
+		s = v.String()
+	}
+	return true, encodeString(w, String, s)
+}
+
+func encodeLength(w io.Writer, t byte, z uint64) error {
+	switch {
+	default:
+		return fmt.Errorf("cbor: too large")
+	case z < uint64(Len1):
+		binary.Write(w, binary.BigEndian, t|byte(z))
+	case z <= math.MaxUint8:
+		binary.Write(w, binary.BigEndian, t|Len1)
+		binary.Write(w, binary.BigEndian, uint8(z))
+	case z <= math.MaxUint16:
+		binary.Write(w, binary.BigEndian, t|Len2)
+		binary.Write(w, binary.BigEndian, uint16(z))
+	case z <= math.MaxUint32:
+		binary.Write(w, binary.BigEndian, t|Len4)
+		binary.Write(w, binary.BigEndian, uint32(z))
+	case z <= math.MaxUint64:
+		binary.Write(w, binary.BigEndian, t|Len8)
+		binary.Write(w, binary.BigEndian, uint64(z))
+	}
+	return nil
+}
+
+func encodeString(w io.Writer, t byte, v string) error {
+	r := []byte(v)
+	if err := encodeLength(w, t, uint64(len(r))); err != nil {
+		return err
+	}
+	if len(r) > 0 {
+		_, err := w.Write(r)
+		return err
+	}
+	return nil
+}
+
+func encodeNumber(w io.Writer, t byte, v uint64) error {
+	switch {
+	default:
+		return fmt.Errorf("cbor: out of range")
+	case v < uint64(Len1):
+		binary.Write(w, binary.BigEndian, t|byte(v))
+	case v <= math.MaxUint8:
+		binary.Write(w, binary.BigEndian, t|Len1)
+		binary.Write(w, binary.BigEndian, uint8(v))
+	case v <= math.MaxUint16:
+		binary.Write(w, binary.BigEndian, t|Len2)
+		binary.Write(w, binary.BigEndian, uint16(v))
+	case v <= math.MaxUint32:
+		binary.Write(w, binary.BigEndian, t|Len4)
+		binary.Write(w, binary.BigEndian, uint32(v))
+	case v <= math.MaxUint64:
+		binary.Write(w, binary.BigEndian, t|Len8)
+		binary.Write(w, binary.BigEndian, uint64(v))
+	}
+	return nil
+}
--- cbor_test.go
+++ cbor_test.go
@@ -1,193 +1,62 @@
 package cbor
 
 import (
-	"bytes"
-	"encoding/hex"
 	"fmt"
 	"net/url"
 	"testing"
 	"time"
 )
 
-type Sample struct {
-	In  interface{}
-	Out string
-}
-
-var sampleTimes = []Sample{
-	{time.Date(2013, 3, 21, 20, 4, 0, 0, time.UTC), "c074323031332d30332d32315432303a30343a30305a"},
-	{time.Date(2017, 2, 28, 20, 38, 0, 0, time.UTC), "c074323031372d30322d32385432303a33383a30305a"},
-}
-
-var sampleURIs = []Sample{
-	{
-		In:  url.URL{Scheme: "http", Host: "www.example.com"},
-		Out: "d82076687474703a2f2f7777772e6578616d706c652e636f6d",
-	},
-	{
-		In:  url.URL{Scheme: "http", Host: "www.google.com", RawQuery: "q=golang"},
-		Out: "d820781e687474703a2f2f7777772e676f6f676c652e636f6d3f713d676f6c616e67",
-	},
-}
-
-var sampleOthers = []Sample{
-	{false, "f4"},
-	{true, "f5"},
-	{nil, "f7"},
-}
-
-var sampleStrings = []Sample{
-	{"", "60"},
-	{"a", "6161"},
-	{"IETF", "6449455446"},
-	{"\"\\", "62225c"},
-}
-
-var sampleUints = []Sample{
-	{0, "00"},
-	{1, "01"},
-	{10, "0a"},
-	{23, "17"},
-	{24, "1818"},
-	{25, "1819"},
-	{100, "1864"},
-	{1000, "1903e8"},
-	{1000000, "1a000f4240"},
-	{1000000000000, "1b000000e8d4a51000"},
-}
-
-var sampleInts = []Sample{
-	{-1, "20"},
-	{-10, "29"},
-	{-100, "3863"},
-	{-1000, "3903e7"},
-}
-
-var sampleFloats = []Sample{
-	//{0.0, "f90000"},
-	//{-0.0, "f98000"},
-	//{1.0, "f93c00"},
-	//{1.5, "f93e00"},
-	//{65504.0, "f97bff"},
-	//{5.960464477539063e-8, "f90001"},
-	//{0.00006103515625, "f90400"},
-	//{-4.0, "f9c400"},
-	{float64(1.1), "fb3ff199999999999a"},
-	{float32(100000.0), "fa47c35000"},
-	{float32(3.4028234663852886e+38), "fa7f7fffff"},
-	{float64(1.0e+300), "fb7e37e43c8800759c"},
-	{float64(-4.1), "fbc010666666666666"},
-}
-
-var sampleArrays = []Sample{
-	{[]int{1, 2, 3}, "83010203"},
-	{[]interface{}{1, []int{2, 3}, []int{4, 5}}, "8301820203820405"},
-	{[]interface{}{"a", map[string]string{"b": "c"}}, "826161bf61626163ff"},
-}
-
-var sampleMaps = []Sample{
-	{map[string]interface{}{}, "a0"},
-	{map[int]int{1: 2, 3: 4}, "a201020304"},
-	{map[string]interface{}{"a": 1, "b": []int{2, 3}}, "a26161016162820203"},
-	{map[string]interface{}{"Fun": true, "Amt": -2}, "bf6346756ef563416d7421ff"},
-}
-
-func TestStructs(t *testing.T) {
-	type File struct {
-		Name     string
-		Location string
-		Lastmod  time.Time
-		Size     int
+func TestMarshal(t *testing.T) {
+	data := []struct {
+		Value interface{}
+		Want  string
+	}{
+		{Value: uint(0), Want: "0x00"},
+		{Value: uint(1), Want: "0x01"},
+		{Value: uint(10), Want: "0x0a"},
+		{Value: uint(23), Want: "0x17"},
+		{Value: uint(24), Want: "0x1818"},
+		{Value: uint(25), Want: "0x1819"},
+		{Value: uint(100), Want: "0x1864"},
+		{Value: uint(1000), Want: "0x1903e8"},
+		{Value: uint(1000000), Want: "0x1a000f4240"},
+		{Value: int(-1), Want: "0x20"},
+		{Value: int(-500), Want: "0x3901f3"},
+		{Value: int(-10), Want: "0x29"},
+		{Value: int(-100), Want: "0x3863"},
+		{Value: int(-1000), Want: "0x3903e7"},
+		{Value: true, Want: "0xf5"},
+		{Value: false, Want: "0xf4"},
+		{Value: "", Want: "0x60"},
+		{Value: "a", Want: "0x6161"},
+		{Value: "IETF", Want: "0x6449455446"},
+		{Value: float32(0.0), Want: "0xf90000"},
+		{Value: float32(-0.0), Want: "0xf98000"},
+		{Value: float32(1.0), Want: "0xf93c00"},
+		{Value: float64(1.1), Want: "0xfb3ff199999999999a"},
+		{Value: float32(1.5), Want: "0xf93e00"},
+		{Value: float32(65504.0), Want: "0xf97bff"},
+		{Value: float32(100000.0), Want: "0xfa47c35000"},
+		{Value: []int{}, Want: "0x80"},
+		{Value: []int{1, 2, 3}, Want: "0x83010203"},
+		{Value: map[string]interface{}{"a": 1, "b": []int{2, 3}}, Want: "0xa26161016162820203"},
+		{Value: []interface{}{"a", map[string]string{"b": "c"}}, Want: "0x826161a161626163"},
+		{Value: time.Date(2013, 3, 21, 20, 4, 0, 0, time.UTC), Want: "0xc074323031332d30332d32315432303a30343a30305a"},
+		{Value: &url.URL{Scheme: "http", Host: "www.example.com"}, Want: "0xd82076687474703a2f2f7777772e6578616d706c652e636f6d"},
 	}
-	src := File{"cbor_test.go", "/tmp", time.Now(), 35673}
-	buf, err := Marshal(src)
-	if err != nil {
-		t.Errorf("fail to marshal %+v: %s", src, err)
-		return
-	}
-	dst := new(File)
-	if _, err := Unmarshal(buf, dst); err != nil {
-		t.Errorf("fail to unmarshal %+v: %s", dst, err)
-	}
-}
-
-func TestMaps(t *testing.T) {
-	runTests(t, sampleMaps)
-}
-
-func TestArrays(t *testing.T) {
-	runTests(t, sampleArrays)
-}
-
-func TestTimes(t *testing.T) {
-	runTests(t, sampleTimes)
-}
-
-func TestURIs(t *testing.T) {
-	runTests(t, sampleURIs)
-}
-
-func TestStrings(t *testing.T) {
-	runTests(t, sampleStrings)
-}
-
-func TestOthers(t *testing.T) {
-	runTests(t, sampleOthers)
-}
-
-func TestInts(t *testing.T) {
-	runTests(t, sampleInts)
-}
-
-func TestUints(t *testing.T) {
-	runTests(t, sampleUints)
-}
-
-func TestFloats(t *testing.T) {
-	runTests(t, sampleFloats)
-}
-
-func runTests(t *testing.T, s []Sample) {
-	sample := map[string]func([]Sample, *testing.T){
-		"marshal":   testMarshalSample,
-		"unmarshal": testUnmarshalSample,
-	}
-	for n, f := range sample {
-		t.Run(n, func(t *testing.T) {
-			f(s, t)
-		})
-	}
-}
-
-func testMarshalSample(sample []Sample, t *testing.T) {
-	for i, s := range sample {
-		buf, err := Marshal(s.In)
+	for _, d := range data {
+		got, err := Marshal(d.Value)
 		if err != nil {
-			t.Errorf("%03d) fail to marshal %v (%s)", i+1, s.In, err)
+			t.Error(err)
 			continue
 		}
-		other, _ := hex.DecodeString(s.Out)
-		if !bytes.Equal(buf, other) {
-			t.Errorf("%03d) want: %x, got: %x", i+1, other, buf)
-		}
-	}
-}
-
-func testUnmarshalSample(sample []Sample, t *testing.T) {
-	for i, s := range sample {
-		buf, err := hex.DecodeString(s.Out)
-		if err != nil {
-			t.Errorf("invalid encoded cbor item %s: %s", s.Out, err)
-			continue
-		}
-
-		var other interface{}
-		if _, err := Unmarshal(buf, &other); err != nil {
-			t.Errorf("%03d) fail to unmarshal %#x, should be %v (%s)", i+1, buf, s.In, err)
-			continue
-		}
-		if fmt.Sprintf("%v", other) != fmt.Sprintf("%v", s.In) {
-			t.Errorf("%03d) want: %v, got: %v", i+1, s.In, other)
+		if s := fmt.Sprintf("%#x", got); d.Want != s {
+			r := "%v => want: %s, got: %s"
+			if _, ok := d.Value.(fmt.Stringer); ok {
+				r = "%s => want: %s, got: %s"
+			}
+			t.Errorf(r, d.Value, d.Want, s)
 		}
 	}
 }
