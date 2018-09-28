23dd93b5776c76fd94f10c29a019999efcf0397c
nico <brun.nicolas21@gmail.com>
2018-09-28 09:06:00 +0200

    debug cbor data

--- cbor.go
+++ cbor.go
@@ -1,8 +1,12 @@
 package cbor
 
 import (
+	"bufio"
+	"bytes"
+	"encoding/binary"
 	"errors"
 	"fmt"
+	"io"
 )
 
 var (
@@ -54,3 +58,121 @@ const (
 	Len4
 	Len8
 )
+
+func Debug(w io.Writer, bs []byte) error {
+	return DebugReader(w, bytes.NewReader(bs))
+}
+
+func DebugReader(w io.Writer, r io.Reader) error {
+	return debugReader(w, bufio.NewReader(r))
+}
+
+func debugReader(w io.Writer, r *bufio.Reader) error {
+	b, err := r.ReadByte()
+	if err != nil {
+		return err
+	}
+
+	switch m, a := b&0xE0, b&0x1F; m {
+	case Uint:
+		err = debugUint(w, r, a)
+	case Int:
+		err = debugInt(w, r, a)
+	case Bin:
+	case String:
+		err = debugString(w, r, a)
+	case Array:
+		err = debugArray(w, r, a)
+	case Map:
+		err = debugMap(w, r, a)
+	case Tag:
+	case Other:
+		err = debugOther(w, r, a)
+	}
+	return nil
+}
+
+func debugArray(w io.Writer, r io.Reader, a byte) error {
+	return nil
+}
+
+func debugMap(w io.Writer, r io.Reader, a byte) error {
+	return nil
+}
+
+func debugOther(w io.Writer, r io.Reader, a byte) error {
+	return nil
+}
+
+func debugString(w io.Writer, r io.Reader, a byte) error {
+	var (
+		size int
+		err  error
+	)
+	switch a {
+	case Len1:
+		var v uint8
+		err = binary.Read(r, binary.BigEndian, &v)
+		size = int(v)
+	case Len2:
+		var v uint16
+		err = binary.Read(r, binary.BigEndian, &v)
+		size = int(v)
+	case Len4:
+		var v uint32
+		err = binary.Read(r, binary.BigEndian, &v)
+		size = int(v)
+	case Len8:
+		var v uint64
+		err = binary.Read(r, binary.BigEndian, &v)
+		size = int(v)
+	default:
+		size = int(a)
+	}
+	if err != nil {
+		return err
+	}
+	bs := make([]byte, size)
+	if _, err := io.ReadFull(r, bs); err != nil {
+		return err
+	}
+	fmt.Fprintf(w, "string (%03b_%05b): %s\n", String>>5, a, string(bs))
+	return nil
+}
+
+func debugUint(w io.Writer, r io.Reader, a byte) error {
+	var (
+		err  error
+		item uint
+	)
+	switch a {
+	case Len1:
+		var v uint8
+		err = binary.Read(r, binary.BigEndian, &v)
+		item = uint(v)
+	case Len2:
+		var v uint16
+		err = binary.Read(r, binary.BigEndian, &v)
+		item = uint(v)
+	case Len4:
+		var v uint32
+		err = binary.Read(r, binary.BigEndian, &v)
+		item = uint(v)
+	case Len8:
+		var v uint64
+		err = binary.Read(r, binary.BigEndian, &v)
+		item = uint(v)
+	default:
+		item = uint(a)
+	}
+	if err != nil {
+		return err
+	}
+	fmt.Fprintf(w, "uint (%03b_%05b): %d\n", Uint>>5, a, item)
+	return nil
+}
+
+func debugInt(w io.Writer, r io.Reader, a byte) error {
+	fmt.Fprintf(w, "int (%03b_%05b): %d\n", Int>>5, a, 0)
+	return nil
+}
